816e9635997ab82d6db68e7f53c06fc27cb069a2
wolfricoz <ricoisdenaam@gmail.com>
2025-02-10 05:50:03 +0100

    Added permission checks

--- modules/template.py
+++ modules/template.py
@@ -20,48 +20,48 @@ class Server(commands.GroupCog):
 
     @app_commands.command(name="template", description="Applies a template to your server and archives old channels. 'help' for templates")
     @app_commands.autocomplete(template=auto_complete_templates)
+    @app_commands.checks.has_permissions(administrator=True)
     async def template(self, interaction: discord.Interaction, template: str):
         await interaction.response.defer(thinking=False)
         if len(interaction.guild.members) >= 10:
             await interaction.followup.send("[Safeguard] Guild has more than 10 users, command will not run")
             return
+        if interaction.user != interaction.guild.owner:
+            return await interaction.followup.send("Only the owner of the server can run this command", ephemeral=True)
         archive_name = f"archive-{datetime.now().strftime('%d-%m-%Y')}"
-        if interaction.user == interaction.guild.owner:
-            def check(m):
-                return m.content is not None and m.channel == interaction.channel
-            confirm = True
-            try:
-                with open(f'templates/{template}.json', 'r') as f:
-                    while confirm is True:
-                        desc = "to apply this template, please type **'confirm'**"
+        def check(m):
+            return m.content is not None and m.channel == interaction.channel
+        confirm = True
+        try:
+            with open(f'templates/{template}.json', 'r') as f:
+                while confirm is True:
+                    desc = "to apply this template, please type **'confirm'**"
+                    embed = discord.Embed(title=f"Apply template?", description=desc)
+                    conf = await interaction.channel.send(embed=embed)
+                    msg = await self.bot.wait_for('message', check=check)
+                    if "confirm" in msg.content.lower():
+                        desc = "Confirmation given, applying template now."
                         embed = discord.Embed(title=f"Apply template?", description=desc)
-                        conf = await interaction.channel.send(embed=embed)
-                        msg = await self.bot.wait_for('message', check=check)
-                        if "confirm" in msg.content.lower():
-                            desc = "Confirmation given, applying template now."
-                            embed = discord.Embed(title=f"Apply template?", description=desc)
-                            confirm = False
-                            await conf.edit(embed=embed)
-                            await msg.delete()
-                            sleep(3)
-                    data = json.load(f)
-                    archive: discord.Category = discord.utils.get(interaction.guild.categories, name=archive_name)
-                    if archive is  None:
-                        archive = await interaction.guild.create_category(name=archive_name)
-                    await Templater.apply(interaction, archive, data)
-                    desc = "to clear the archive please use **/server archivepurge**"
-                    embed = discord.Embed(title=f"Template successfully applied!", description=desc)
-                    await conf.edit(embed=embed)
+                        confirm = False
+                        await conf.edit(embed=embed)
+                        await msg.delete()
+                        sleep(3)
+                data = json.load(f)
+                archive: discord.Category = discord.utils.get(interaction.guild.categories, name=archive_name)
+                if archive is  None:
+                    archive = await interaction.guild.create_category(name=archive_name)
+                await Templater.apply(interaction, archive, data)
+                desc = "to clear the archive please use **/server archivepurge**"
+                embed = discord.Embed(title=f"Template successfully applied!", description=desc)
+                await conf.edit(embed=embed)
 
-            except Exception as e:
-                print(e)
-                templates = []
-                for file in os.listdir('templates'):
-                    templates.append(file[:-5])
-                temps = "\n- ".join(templates)
-                await interaction.followup.send(f"- {temps}")
-        else:
-            await interaction.followup.send("[Safeguard] No permission")
+        except Exception as e:
+            print(e)
+            templates = []
+            for file in os.listdir('templates'):
+                templates.append(file[:-5])
+            temps = "\n- ".join(templates)
+            await interaction.followup.send(f"- {temps}")
 
     @app_commands.command(name="archivepurge", description="removes ALL archived channels, this can NOT be undone.")
     @app_commands.checks.has_permissions(administrator=True)
